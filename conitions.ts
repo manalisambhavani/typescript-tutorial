0f09faaa2d6e3e3789cb76a0103d0d1bfd44c8ec
Manali <manalisambhavani2015@gmail.com>
2025-07-22 16:10:27 +0530

    newly added

--- conitions.ts
+++ conitions.ts
@@ -6,14 +6,14 @@
  */
 
 let age = 35,
-  ticketAmount = 0;
+    ticketAmount = 0;
 
 if (age >= 10 && age <= 20) {
-  ticketAmount = 10;
+    ticketAmount = 10;
 } else if (age >= 21 && age <= 30) {
-  ticketAmount = 20;
+    ticketAmount = 20;
 } else if (age >= 31 && age <= 40) {
-  ticketAmount = 30;
+    ticketAmount = 30;
 }
 
 console.log({ ticketAmount });
---
+++ odd-even.ts
@@ -0,0 +1,7 @@
+const x = 0;
+
+if (x % 2 == 0) {
+  console.log(" This is Even Number");
+} else {
+  console.log("This is Odd Number");
+}
